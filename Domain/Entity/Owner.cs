3d43446f9b5a28fa4db3251790fe58bea30a34df
Boris <bo90ris@gmail.com>
2025-05-13 10:19:10 +0300

    added owner class

--- Domain/Entity/Owner.cs
+++ Domain/Entity/Owner.cs
@@ -1,6 +1,32 @@
+using Domain.Base;
+using Domain.Helpers;
+
 namespace Domain.Entity;
 
-public class Owner
+public class Owner : IEntity<Guid>
 {
+    /// <summary>
+    /// ИД пользователя
+    /// </summary>
+    public Guid Id { get; set; }
     
+    /// <summary>
+    /// Тип пользователя
+    /// </summary>
+    public UserType userType { get; set; }
+    
+    /// <summary>
+    /// Дата создания
+    /// </summary>
+    public DateTime dateCreated { get; set; } = DateTime.Now;
+    
+    /// <summary>
+    /// Дата изменения
+    /// </summary>
+    public DateTime? dateModify { get; set; }
+    
+    /// <summary>
+    /// признак удаления
+    /// </summary>
+    public bool isDeleted { get; set; } = false;
 }
